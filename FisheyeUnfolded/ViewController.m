86ea50b7cf8f8d59de058bb37566fcf62b0146fe
Lal <mfs@gurubj.com>
2017-02-25 18:31:30 +0800

    添加视频和图片标定代码

--- FisheyeUnfolded/ViewController.m
+++ FisheyeUnfolded/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "Fisheye.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -17,6 +19,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *image = [UIImage imageNamed:@"cat-cap"];
+    _imageView.image = image;
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        Fisheye *fisheye = [[Fisheye alloc] init];
+        //视频标定
+//        NSString *imagePath = [[NSBundle mainBundle] pathForResource:@"20170217172421.avi" ofType:nil];
+//        [fisheye calibrationWithVedioPath:imagePath];
+        //图片标定
+        [fisheye calibrationWithImageNameArray:@[@"cat-cap1",@"cat-cap2",@"cat-cap3",
+                                                 @"cat-cap4",@"cat-cap5",@"cat-cap6"]];
+
+        UIImage *undistortImage = [fisheye undistortImage:image];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            _imageView.image = undistortImage;
+        });
+    });
+    
 }
 
 
